857c5ca31fc3dc77f18b3149d6b48992be0421d6
Gregory Owens <gregory.owens@alumni.ubc.ca>
2018-04-24 11:34:41 -0700

    Testing no BDMs

--- run_slim_sim_v2.pl
+++ run_slim_sim_v2.pl
@@ -9,15 +9,15 @@ use Parallel::ForkManager;
 
 
 #NOTE: This is the path to your nonWF slim program
-my $slim = "slim";
+my $slim = "/home/owens/working/SLiM/bin/slim";
 #PARALLEL EXECUTION
-my $pm = new Parallel::ForkManager(3);
+my $pm = new Parallel::ForkManager(11);
 
 my @chosen_k = qw(500 1000 2000 5000 10000);
-my @chosen_m = qw(0.001 0.005 0.01);
+my @chosen_m = qw(0.01);
 my @chosen_div_n = qw(100);
-my @chosen_div_s = qw(0.0001 0.0005 0.001 0.005);
-my $reps =10; 
+my @chosen_div_s = qw(0.002 0.003 0.004);
+my $reps =5; 
 
 foreach my $chosen_k (@chosen_k){
 	foreach my $chosen_m (@chosen_m){
@@ -27,7 +27,7 @@ foreach my $chosen_k (@chosen_k){
 					$pm->start and next;
 					my $output = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s adaptive_introgression_WF_BDM_CMD.slim | tail -n 1`;
 					chomp($output);
-					print("$output\tOUTPUT\n");
+					print("$output\n");
 					$pm->finish
 				}
 			}
