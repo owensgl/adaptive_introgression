c70949c811bde288f4b77c30a30780c0eba97c4d
Gregory Owens <gregory.owens@alumni.ubc.ca>
2018-05-17 11:31:10 -0700

    Added multi file output

--- run_slim_sim_v3.pl
+++ run_slim_sim_v3.pl
@@ -13,7 +13,7 @@ my $slim = "/home/owens/working/SLiM/bin/slim";
 #PARALLEL EXECUTION
 my $pm = new Parallel::ForkManager(20);
 
-my @chosen_k = qw(1000 2000 4000);
+my @chosen_k = qw(100 1000 2000 4000);
 my @chosen_m = qw(0.01 0.05 0.1);
 my @chosen_div_n = qw(100);
 my @chosen_div_s = qw(0.004 0.008 0.016);
@@ -31,30 +31,36 @@ foreach my $chosen_k (@chosen_k){
 				foreach my $rep (1..$reps){
 					foreach my $chosen_mutation_rate (@chosen_mutation_rate){
 						foreach my $chosen_delta (@chosen_delta){
+							foreach my $chosen_bdm_n (@chosen_bdm_n){
+					                        foreach my $chosen_bdm_s (@chosen_bdm_s){
 							
-							$pm->start and next;
-							srand();
-							my $seed = int(rand(100000000000));
-							my $output_1 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_bdm_s=$chosen_bdm_s -d d_bdm_n=$chosen_bdm_n -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$burn_in_gen -d d_shift_gen=$shift_gen adaptive_introgression_WF_BDM_CMD.slim`;
-							chomp($output_1);
-							my @lines = split(/\n/,$output_1);
-							foreach my $line (@lines){
-								if ($line =~ m/^OUT/){
-									print("$seed\ttest\t$line\n");
+									$pm->start and next;
+									srand();
+									my $filename = "output_${chosen_k}_${chosen_m}_${burn_in_gen}_${chosen_div_n}_${chosen_div_s}_${chosen_bdm_n}_${chosen_bdm_s}_${chosen_mutation_rate}_${chosen_delta}_${rep}.txt";
+									open (my $fh,'>', $filename);
+									my $seed = int(rand(100000000000));
+									my $output_1 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_bdm_s=$chosen_bdm_s -d d_bdm_n=$chosen_bdm_n -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$burn_in_gen -d d_shift_gen=$shift_gen adaptive_introgression_WF_BDM_CMD.slim`;
+									chomp($output_1);
+									my @lines = split(/\n/,$output_1);
+									foreach my $line (@lines){
+										if ($line =~ m/^OUT/){
+											print $fh "$seed\ttest\t$line\n";
+										}
+									}
+									my $longer_gen = $burn_in_gen + $shift_gen;
+                                              		          	my $output_2 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_bdm_s=$chosen_bdm_s -d d_bdm_n=$chosen_bdm_n -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$longer_gen -d d_shift_gen=0 adaptive_introgression_WF_BDM_CMD.slim`;
+                                                        		chomp($output_2);
+		                                                        @lines = split(/\n/,$output_2);
+                		                                        foreach my $line (@lines){
+                                		                                if ($line =~ m/^OUT/){
+											if ($line =~ m/burn_in/){next;}
+                                                                		        print $fh "$seed\tcontrol\t$line\n";
+		                                                                }
+                		                                        }
+									close $fh;
+									$pm->finish
 								}
 							}
-							my $longer_gen = $burn_in_gen + $shift_gen;
-                                                        my $output_2 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_bdm_s=$chosen_bdm_s -d d_bdm_n=$chosen_bdm_n -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$longer_gen -d d_shift_gen=0 adaptive_introgression_WF_BDM_CMD.slim`;
-                                                        chomp($output_2);
-                                                        @lines = split(/\n/,$output_2);
-                                                        foreach my $line (@lines){
-                                                                if ($line =~ m/^OUT/){
-									if ($line =~ m/burn_in/){next;}
-                                                                        print("$seed\tcontrol\t$line\n");
-                                                                }
-                                                        }
-
-							$pm->finish
 						}
 					}
 				}
