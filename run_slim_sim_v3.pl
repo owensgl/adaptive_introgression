a72e69183ae54d642ecdc7c66a2bce6b9924cdba
ksamuk <ksamuk@gmail.com>
2018-05-17 13:53:45 -0400

    preparing for parameter space trial

--- run_slim_sim_v3.pl
+++ run_slim_sim_v3.pl
@@ -11,15 +11,17 @@ use Parallel::ForkManager;
 #NOTE: This is the path to your nonWF slim program
 my $slim = "/home/owens/working/SLiM/bin/slim";
 #PARALLEL EXECUTION
-my $pm = new Parallel::ForkManager(11);
+my $pm = new Parallel::ForkManager(20);
 
-my @chosen_k = qw(500 1000 2000);
-my @chosen_m = qw(0.01);
+my @chosen_k = qw(1000 2000 4000);
+my @chosen_m = qw(0.01 0.05 0.1);
 my @chosen_div_n = qw(100);
-my @chosen_div_s = qw(0.002 0.003 0.004);
-my @chosen_mutation_rate = qw(1e-7);
-my @chosen_delta = qw(0.01 0.1);
-my $reps =5; 
+my @chosen_div_s = qw(0.004 0.008 0.016);
+my @chosen_bdm_n = qw(10);
+my @chosen_bdm_s = qw(0.001 0.01 0.1);
+my @chosen_mutation_rate = qw(1e-7 1e-6);
+my @chosen_delta = qw(0.01 0.05 0.1);
+my $reps =100; 
 my $burn_in_gen = 3500;
 my $shift_gen = 100;
 foreach my $chosen_k (@chosen_k){
@@ -32,8 +34,8 @@ foreach my $chosen_k (@chosen_k){
 							
 							$pm->start and next;
 							srand();
-							my $seed = int(rand(10000000));
-							my $output_1 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$burn_in_gen -d d_shift_gen=$shift_gen adaptive_introgression_WF_BDM_CMD.slim`;
+							my $seed = int(rand(100000000000));
+							my $output_1 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_bdm_s=$chosen_bdm_s -d d_bdm_n=$chosen_bdm_n -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$burn_in_gen -d d_shift_gen=$shift_gen adaptive_introgression_WF_BDM_CMD.slim`;
 							chomp($output_1);
 							my @lines = split(/\n/,$output_1);
 							foreach my $line (@lines){
@@ -42,7 +44,7 @@ foreach my $chosen_k (@chosen_k){
 								}
 							}
 							my $longer_gen = $burn_in_gen + $shift_gen;
-                                                        my $output_2 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$longer_gen -d d_shift_gen=0 adaptive_introgression_WF_BDM_CMD.slim`;
+                                                        my $output_2 = `$slim -d d_pop_size=$chosen_k -d d_m=$chosen_m -d d_div_sel_n=$chosen_div_n -d d_div_sel_s=$chosen_div_s -d d_bdm_s=$chosen_bdm_s -d d_bdm_n=$chosen_bdm_n -d d_mutation_rate=$chosen_mutation_rate -d d_delta=$chosen_delta -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$longer_gen -d d_shift_gen=0 adaptive_introgression_WF_BDM_CMD.slim`;
                                                         chomp($output_2);
                                                         @lines = split(/\n/,$output_2);
                                                         foreach my $line (@lines){
