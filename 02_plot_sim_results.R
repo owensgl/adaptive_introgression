1d39bb8e9d3948f4f3e24946fa846ed1a08fcb91
Gregory Owens <gregoryowens@vlsb-4151-1.biol.berkeley.edu>
2018-06-18 10:19:32 -0700

    New version of slim that has both test and burnin

--- 02_plot_sim_results.R
+++ 02_plot_sim_results.R
@@ -13,8 +13,8 @@ summary_df <- summary_df %>%
   mutate(bdm_s = strsplit(as.character(param_string), split = "_") %>% unlist %>% .[7]) 
 
 # not interested in burn in phase
-summary_df <- summary_df %>%
-  filter(phase != "burn_in")
+#summary_df <- summary_df %>%
+#  filter(phase != "burn_in")
 
 # summarise the summaries (!)
 # percent_diff = treatment (shift) - control (no shift)
@@ -22,19 +22,37 @@ summary_df <- summary_df %>%
 
 sum_df <- summary_df  %>%
   group_by(ne, m, div_sel_n, div_sel_s, mu, delta, bdm_n, bdm_s, seed, rep) %>%
-  summarise(p1_home_percent_diff = p1_home_percent[1] - p1_home_percent[2],
-            p2_home_percent_diff = p2_home_percent[1] - p2_home_percent[2],
-            fit_dif_diff = fit_dif[1] - fit_dif[2])
+  summarise(p1_home_percent_diff = p1_home_percent[2] - p1_home_percent[3],
+            p2_home_percent_diff = p2_home_percent[2] - p2_home_percent[3],
+            fit_dif_diff = fit_dif[2] - fit_dif[3],
+            pre_fit = fit_dif[1],
+            test_post_fit =fit_dif[2],
+            control_post_fit = fit_dif[3],
+            pre_p1 = p1_home_percent[1],
+            test_post_p1 = p1_home_percent[2],
+            control_post_p1 = p1_home_percent[3])
 
 # plot some results
 summary_df %>%
   mutate(ne_m = ne*m) %>%
+  filter(div_sel_s == 0.008, ne==1000, m==0.05) %>%
   ggplot(aes(y = p1_home_percent, x = as.factor(delta), color = as.factor(treatment)))+
   geom_boxplot()+
-  facet_grid(ne_m ~ div_sel_s)
+  facet_grid(ne*m ~ div_sel_s)
 
 
-sum_df %>%
-  ggplot(aes(y = p1_home_percent_diff, x = delta, fill = delta))+
-  geom_boxplot()+
-  facet_grid(ne*m ~ mu)
+sum_df %>% filter(bdm_s == 0.001) %>% 
+  #filter(div_sel_s == 0.008, ne==1000, m==0.1) %>%
+  ggplot(aes(y = control_post_p1, x = test_post_p1, color = delta))+
+  geom_point(alpha=0.5) +
+  geom_abline(slope=1) +
+  facet_grid(ne*m ~ div_sel_s, labeller = label_both) +
+  theme_bw()
+
+sum_df %>% filter(bdm_s == 0.001) %>% 
+  filter(div_sel_s == 0.008, ne==1000, m==0.1) %>%
+  filter(pre_fit > 0.4) %>%
+  ggplot(aes(p1_home_percent_diff, fill = delta, group = delta))+
+  geom_density() + facet_wrap(~delta, labeller = label_both) +
+  theme_bw()
+  
