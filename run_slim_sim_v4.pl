73f30b37e7187cc3e6bfc75c331f9df0b0230f2b
Gregory Owens <gregory.owens@alumni.ubc.ca>
2018-06-28 13:24:27 -0700

    New streamlined runner

--- run_slim_sim_v4.pl
+++ run_slim_sim_v4.pl
@@ -11,13 +11,13 @@ use Parallel::ForkManager;
 #NOTE: This is the path to your nonWF slim program
 my $slim = "/home/owens/working/SLiM/bin/slim";
 #PARALLEL EXECUTION
-my $pm = new Parallel::ForkManager(1);
+my $pm = new Parallel::ForkManager(11);
 my %p;
-$p{k} = 1000; #Population size
+$p{pop_size} = 1000; #Population size
 $p{m} = 0.01; #Mutation rate
 $p{total_div} = 0.9; #Total amount of reproductive isolation amongst all divergently selected sites.
-$p{div_n} = 100; #Number of divergently selected alleles
-$p{div_s} = $p{total_div}/$p{div_n}; #Strength of selection at divergently selected alleles
+$p{div_sel_n} = 100; #Number of divergently selected alleles
+$p{div_sel_s} = $p{total_div}/$p{div_sel_n}; #Strength of selection at divergently selected alleles
 $p{bdm_n} = 10; #Number of BDM alleles, must be divisible by 2
 $p{bdm_s} = 0.0; #Strength of selection at BDMs
 $p{mutation_rate} = 1e-7; #Mutation rate for adaptive alleles
@@ -25,16 +25,19 @@ $p{qtl_sd} = 1; #Standard deviation of qtl
 $p{fitness_sd} = 2.0; #Standard deviation of fitness landscape.
 $p{delta} = 0.1; #How fast the optimum shifts after the burn in
 my $reps =10;  #Number of repetitions per set of parameters
-$p{burn_in_gen} = 1000; #Number of generations of burn in before shift
+$p{burn_in_gen} = 10000; #Number of generations of burn in before shift
 $p{shift_gen} = 100; #Number of generations of shifting optimum.
 
 my @parameters = sort keys %p;
-my @p_changes = qw(0.1 0.5 1 2 10); #Numbers to multiply the starting values by. 
+my @p_changes = qw(0.1 0.2 0.5 0.75 1 1.5 2 5 10); #Numbers to multiply the starting values by. 
 my %varying_p; #each parameter that is going to vary individually. 
-$varying_p{m}++;
-#$varying_p{div_s}++;
-#$varying_p{mutation_rate}++;
-#$varying_p{delta}++;
+
+#PARAMETERS TO VARY
+$varying_p{delta}++;
+$varying_p{qtl_sd}++;
+$varying_p{mutation_rate}++;
+$varying_p{div_sel_n}++;
+$varying_p{fitness_sd}++;
 
 my $parameter_file = "parameter_file.txt";
 open (my $ph, '>', $parameter_file);
@@ -50,7 +53,7 @@ foreach my $varying_parameter (sort keys %varying_p){
 		my %tmp_p = %p;
 		$tmp_p{$varying_parameter} = $p{$varying_parameter} * $p_change; #Multiply the chosen parameter up or down.
 		if ($tmp_p{m} > 0.5){next;} #Skip if migration is greater than panmixis.
-		$tmp_p{div_s} = $tmp_p{total_div}/$tmp_p{div_n};
+		$tmp_p{div_sel_s} = $tmp_p{total_div}/$tmp_p{div_sel_n};
 		foreach my $rep (1..$reps){
 			$pm->start and next;
 			srand();
@@ -66,10 +69,9 @@ foreach my $varying_parameter (sort keys %varying_p){
                         foreach my $parameter (@parameters){
                                 $command_1 .= " -d d_${parameter}=$tmp_p{$parameter}";
                         }
+			$command_1 .= " -d d_rep=$rep";
 			$command_1 .= " -d d_seed=$seed";
-			$command_1 .= " -d d_version=\"burn\" adaptive_introgression_WF_BDM_CMD.slim";
-			print STDERR "$command_1";
-			exit;
+			$command_1 .= ' -d d_version=\"burn\" adaptive_introgression_WF_BDM_CMD.slim';
 			my $output_1 = `$command_1`;
 			chomp($output_1);
                         my @lines = split(/\n/,$output_1);
@@ -83,8 +85,9 @@ foreach my $varying_parameter (sort keys %varying_p){
                         foreach my $parameter (@parameters){
                                 $command_2 .= " -d d_${parameter}=$tmp_p{$parameter}";
                         }
+			$command_2 .= " -d d_rep=$rep";
                         $command_2 .= " -d d_seed=$seed";
-                        $command_2 .= " -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim";
+                        $command_2 .= ' -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim';
 			my $output_2 = `$command_2`;
 			chomp($output_2);
 			@lines = split(/\n/,$output_2);
@@ -97,12 +100,13 @@ foreach my $varying_parameter (sort keys %varying_p){
 			print STDERR "Running control for $filename\n";
                         my $command_3 = "$slim";
                         foreach my $parameter (@parameters){
-				if ($parameter == "burn_in_gen"){next;}
+				if ($parameter eq "burn_in_gen"){next;}
                                 $command_3 .= " -d d_${parameter}=$tmp_p{$parameter}";
                         }
+			$command_3 .= " -d d_rep=$rep";
 			$command_3 .= " -d d_burn_in_gen=$longer_gen";
                         $command_3 .= " -d d_seed=$seed";
-                        $command_3 .= " -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim";
+                        $command_3 .= ' -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim';
                         my $output_3 = `$command_3`;
 			chomp($output_3);
 			@lines = split(/\n/,$output_3);
