f68a8050c9c5e861afccb893db08804725cc8847
Gregory Owens <gregory.owens@alumni.ubc.ca>
2018-06-28 13:04:16 -0700

    Problems with slim, testing it

--- convert_cmd_to_gui.pl
+++ convert_cmd_to_gui.pl
@@ -15,9 +15,10 @@ my $d_seed = "1111";
 my $d_burn_in_gen = "1000";
 my $d_shift_gen = "100";
 my $d_rep = "100";
-
+my $d_fitness_sd = "2.0";
+my $d_qtl_sd = "1";
 my $input_file = $ARGV[0];
 my $output_file = $ARGV[1];
 
 
-system("cat $input_file | sed s/d_version/$d_version/g | sed s/d_pop_size/$d_pop_size/g | sed s/d_div_sel_n/$d_div_sel_n/g | sed s/d_div_sel_s/$d_div_sel_s/g | sed s/d_bdm_n/$d_bdm_n/g | sed s/d_bdm_s/$d_bdm_s/g | sed s/d_mutation_rate/$d_mutation_rate/g | sed s/d_delta/$d_delta/g | sed s/d_seed/$d_seed/g | sed s/d_burn_in_gen/$d_burn_in_gen/g | sed s/d_shift_gen/$d_shift_gen/g | sed s/d_m/$d_m/g | sed s/d_rep/$d_rep/g > $output_file ");
+system("cat $input_file | sed s/d_version/$d_version/g | sed s/d_pop_size/$d_pop_size/g | sed s/d_div_sel_n/$d_div_sel_n/g | sed s/d_div_sel_s/$d_div_sel_s/g | sed s/d_bdm_n/$d_bdm_n/g | sed s/d_bdm_s/$d_bdm_s/g | sed s/d_mutation_rate/$d_mutation_rate/g | sed s/d_delta/$d_delta/g | sed s/d_seed/$d_seed/g | sed s/d_burn_in_gen/$d_burn_in_gen/g | sed s/d_shift_gen/$d_shift_gen/g | sed s/d_m/$d_m/g | sed s/d_rep/$d_rep/g | sed s/d_fitness_sd/$d_fitness_sd/g | sed s/d_qtl_sd/$d_qtl_sd/g > $output_file ");
--- run_slim_sim_v4.pl
+++ run_slim_sim_v4.pl
@@ -13,19 +13,22 @@ my $slim = "/home/owens/working/SLiM/bin/slim";
 #PARALLEL EXECUTION
 my $pm = new Parallel::ForkManager(1);
 my %p;
-$p{k} = 1000;
-$p{m} = 0.01;
-$p{div_n} = 100;
-$p{div_s} = 0.005;
-$p{bdm_n} = 10;
-$p{bdm_s} = 0.0;
-$p{mutation_rate} = 1e-7;
-$p{delta} = 0.1;
-my $reps =10; 
-$p{burn_in_gen} = 1000;
-$p{shift_gen} = 100;
+$p{k} = 1000; #Population size
+$p{m} = 0.01; #Mutation rate
+$p{total_div} = 0.9; #Total amount of reproductive isolation amongst all divergently selected sites.
+$p{div_n} = 100; #Number of divergently selected alleles
+$p{div_s} = $p{total_div}/$p{div_n}; #Strength of selection at divergently selected alleles
+$p{bdm_n} = 10; #Number of BDM alleles, must be divisible by 2
+$p{bdm_s} = 0.0; #Strength of selection at BDMs
+$p{mutation_rate} = 1e-7; #Mutation rate for adaptive alleles
+$p{qtl_sd} = 1; #Standard deviation of qtl 
+$p{fitness_sd} = 2.0; #Standard deviation of fitness landscape.
+$p{delta} = 0.1; #How fast the optimum shifts after the burn in
+my $reps =10;  #Number of repetitions per set of parameters
+$p{burn_in_gen} = 1000; #Number of generations of burn in before shift
+$p{shift_gen} = 100; #Number of generations of shifting optimum.
 
-
+my @parameters = sort keys %p;
 my @p_changes = qw(0.1 0.5 1 2 10); #Numbers to multiply the starting values by. 
 my %varying_p; #each parameter that is going to vary individually. 
 $varying_p{m}++;
@@ -33,20 +36,41 @@ $varying_p{m}++;
 #$varying_p{mutation_rate}++;
 #$varying_p{delta}++;
 
+my $parameter_file = "parameter_file.txt";
+open (my $ph, '>', $parameter_file);
+foreach my $parameter (@parameters){
+        print $ph "$parameter\t";
+
+}
+print $ph "rep";
+close $ph;
+
 foreach my $varying_parameter (sort keys %varying_p){
 	foreach my $p_change (@p_changes){
 		my %tmp_p = %p;
 		$tmp_p{$varying_parameter} = $p{$varying_parameter} * $p_change; #Multiply the chosen parameter up or down.
-		if (($tmp_p{div_s} * $tmp_p{div_n}) >= 1){next;} #Skip if fitness penalty is greater than 1.
 		if ($tmp_p{m} > 0.5){next;} #Skip if migration is greater than panmixis.
+		$tmp_p{div_s} = $tmp_p{total_div}/$tmp_p{div_n};
 		foreach my $rep (1..$reps){
 			$pm->start and next;
 			srand();
-			my $filename = "output_$tmp_p{k}_$tmp_p{m}_$tmp_p{burn_in_gen}_$tmp_p{div_n}_$tmp_p{div_s}_$tmp_p{bdm_n}_$tmp_p{bdm_s}_$tmp_p{mutation_rate}_$tmp_p{delta}_${rep}.txt";
+			my $filename = "output";
+			foreach my $parameter (@parameters){
+				$filename .= "_$tmp_p{$parameter}";
+			}
+			$filename .= "_${rep}.txt";
 			open (my $fh,'>', $filename);
 			my $seed = int(rand(100000000000));
-			print STDERR "Running burn in for output_$tmp_p{k}_$tmp_p{m}_$tmp_p{burn_in_gen}_$tmp_p{div_n}_$tmp_p{div_s}_$tmp_p{bdm_n}_$tmp_p{bdm_s}_$tmp_p{mutation_rate}_$tmp_p{delta}_${rep}\n";
-			my $output_1 = `$slim -d d_pop_size=$tmp_p{k} -d d_m=$tmp_p{m} -d d_div_sel_n=$tmp_p{div_n} -d d_div_sel_s=$tmp_p{div_s} -d d_bdm_s=$tmp_p{bdm_s} -d d_bdm_n=$tmp_p{bdm_n} -d d_mutation_rate=$tmp_p{mutation_rate} -d d_delta=$tmp_p{delta} -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$tmp_p{burn_in_gen} -d d_shift_gen=$tmp_p{shift_gen} -d d_version=\\"burn\\" adaptive_introgression_WF_BDM_CMD.slim`;
+			print STDERR "Running burn in for $filename\n";
+			my $command_1 = "$slim";
+                        foreach my $parameter (@parameters){
+                                $command_1 .= " -d d_${parameter}=$tmp_p{$parameter}";
+                        }
+			$command_1 .= " -d d_seed=$seed";
+			$command_1 .= " -d d_version=\"burn\" adaptive_introgression_WF_BDM_CMD.slim";
+			print STDERR "$command_1";
+			exit;
+			my $output_1 = `$command_1`;
 			chomp($output_1);
                         my @lines = split(/\n/,$output_1);
                         foreach my $line (@lines){
@@ -54,8 +78,14 @@ foreach my $varying_parameter (sort keys %varying_p){
 					print $fh "$seed\ttest\t$line\n";
 				}
 			}
-			print STDERR "Running test shift for output_$tmp_p{k}_$tmp_p{m}_$tmp_p{burn_in_gen}_$tmp_p{div_n}_$tmp_p{div_s}_$tmp_p{bdm_n}_$tmp_p{bdm_s}_$tmp_p{mutation_rate}_$tmp_p{delta}_${rep}\n";
-			my $output_2 = `$slim -d d_pop_size=$tmp_p{k} -d d_m=$tmp_p{m} -d d_div_sel_n=$tmp_p{div_n} -d d_div_sel_s=$tmp_p{div_s} -d d_bdm_s=$tmp_p{bdm_s} -d d_bdm_n=$tmp_p{bdm_n} -d d_mutation_rate=$tmp_p{mutation_rate} -d d_delta=$tmp_p{delta} -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$tmp_p{burn_in_gen} -d d_shift_gen=$tmp_p{shift_gen} -d d_version=\\"test\\" adaptive_introgression_WF_BDM_CMD.slim`;
+			print STDERR "Running test shift for $filename\n";
+                        my $command_2 = "$slim";
+                        foreach my $parameter (@parameters){
+                                $command_2 .= " -d d_${parameter}=$tmp_p{$parameter}";
+                        }
+                        $command_2 .= " -d d_seed=$seed";
+                        $command_2 .= " -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim";
+			my $output_2 = `$command_2`;
 			chomp($output_2);
 			@lines = split(/\n/,$output_2);
 			foreach my $line (@lines){
@@ -64,8 +94,16 @@ foreach my $varying_parameter (sort keys %varying_p){
 				}
 			}
 			my $longer_gen = $tmp_p{burn_in_gen} + $tmp_p{shift_gen};
-			print STDERR "Running control for output_$tmp_p{k}_$tmp_p{m}_$tmp_p{burn_in_gen}_$tmp_p{div_n}_$tmp_p{div_s}_$tmp_p{bdm_n}_$tmp_p{bdm_s}_$tmp_p{mutation_rate}_$tmp_p{delta}_${rep}\n";
-			my $output_3 = `$slim -d d_pop_size=$tmp_p{k} -d d_m=$tmp_p{m} -d d_div_sel_n=$tmp_p{div_n} -d d_div_sel_s=$tmp_p{div_s} -d d_bdm_s=$tmp_p{bdm_s} -d d_bdm_n=$tmp_p{bdm_n} -d d_mutation_rate=$tmp_p{mutation_rate} -d d_delta=$tmp_p{delta} -d d_rep=$rep -d d_seed=$seed -d d_burn_in_gen=$longer_gen -d d_shift_gen=0 -d d_version=\\"test\\" adaptive_introgression_WF_BDM_CMD.slim`;
+			print STDERR "Running control for $filename\n";
+                        my $command_3 = "$slim";
+                        foreach my $parameter (@parameters){
+				if ($parameter == "burn_in_gen"){next;}
+                                $command_3 .= " -d d_${parameter}=$tmp_p{$parameter}";
+                        }
+			$command_3 .= " -d d_burn_in_gen=$longer_gen";
+                        $command_3 .= " -d d_seed=$seed";
+                        $command_3 .= " -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim";
+                        my $output_3 = `$command_3`;
 			chomp($output_3);
 			@lines = split(/\n/,$output_3);
 			foreach my $line (@lines){
