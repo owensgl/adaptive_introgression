435d38e7392e8d06758036dc035cef7b86ba777b
Gregory Owens <gregory.owens@alumni.ubc.ca>
2019-01-07 11:45:39 -0800

    Updating processing script

--- analysis/Processing_output2.R
+++ analysis/Processing_output2.R
@@ -11,6 +11,7 @@ parameter <- "qtlsd"
 parameters_tested <- c("totaldiv","delta","mutationrate","qtlsd","m","divseln")
 
 plots <- list()
+plots_RI <- list()
 for (n in 1:length(parameters_tested)){
   parameter <- parameters_tested[n]
   parameters <- colnames(read_tsv("../parameter_file.txt"))
@@ -55,12 +56,36 @@ for (n in 1:length(parameters_tested)){
 
   plots[[n]] <- plot_dot
   
+  plot_dot_RI <- output2 %>% mutate_("parameter_chosen" = parameter_full) %>%
+    mutate(parameter_chosen = as.numeric(parameter_chosen)) %>%
+    ggplot(.) + geom_point(aes(x=parameter_chosen,y=fit_change,color=version),alpha=0.2) +
+    scale_color_brewer(palette = "Set1",name="Version",labels=c("Control", "Climate change")) + theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+    geom_hline(yintercept=0) +
+    xlab(parameter_print) +
+    ylab("RI loss") +
+    labs(tag = LETTERS[n])
+  
+  plots_RI[[n]] <- plot_dot_RI
+  
+  
+  
 }
 pdf(paste("../figures/",iteration,".points.out2.pdf",sep=""),width=12,height=6)
 
 grid.arrange( plots[[1]],plots[[2]],
               plots[[3]],plots[[4]],
               plots[[5]],plots[[6]],
+              # top = textGrob("Haldane Values",gp=gpar(fontsize=20,font=2)),
+              layout_matrix = rbind(c(1,2,3),
+                                    c(4,5,6)))
+dev.off()
+
+pdf(paste("../figures/",iteration,".points.out2RI.pdf",sep=""),width=12,height=6)
+
+grid.arrange( plots_RI[[1]],plots_RI[[2]],
+              plots_RI[[3]],plots_RI[[4]],
+              plots_RI[[5]],plots_RI[[6]],
               # top = textGrob("Haldane Values",gp=gpar(fontsize=20,font=2)),
               layout_matrix = rbind(c(1,2,3),
                                     c(4,5,6)))
