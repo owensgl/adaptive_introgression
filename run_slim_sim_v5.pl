1b4ef996b1fab133c1603f62cd2510aac1ca84ab
Gregory Owens <gregory.owens@alumni.ubc.ca>
2019-01-29 17:03:29 -0800

    Fixed selection problem

--- run_slim_sim_v5.pl
+++ run_slim_sim_v5.pl
@@ -13,21 +13,21 @@ my $slim = "/home/owens/working/SLiM/bin/slim";
 #PARALLEL EXECUTION
 my $pm = new Parallel::ForkManager(10);
 my %p;
-$p{pop_size} = 1000; #Population size
-$p{m} = 0.01; #Mutation rate
-$p{total_div} = 0.9; #Total amount of reproductive isolation amongst all divergently selected sites.
-$p{div_sel_n} = 100; #Number of divergently selected alleles
-$p{div_sel_s} = $p{total_div}/$p{div_sel_n}; #Strength of selection at divergently selected alleles
-$p{bdm_n} = 10; #Number of BDM alleles, must be divisible by 2
-$p{bdm_s} = 0.0; #Strength of selection at BDMs
-$p{mutation_rate} = 1e-7; #Mutation rate for adaptive alleles
-$p{qtl_sd} = 1; #Standard deviation of qtl 
-$p{fitness_sd} = 2.0; #Standard deviation of fitness landscape.
-$p{delta} = 1; #How fast the optimum shifts after the burn in
-my $reps =100;  #Number of repetitions per set of parameters
-$p{burn_in_gen} = 10000; #Number of generations of burn in before shift
-$p{shift_gen} = 100; #Number of generations of shifting optimum.
-
+$p{"popsize"} = 1000; #Population size
+$p{"m"} = 0.01; #Mutation rate
+$p{"totaldiv"} = 0.9; #Total amount of reproductive isolation amongst all divergently selected sites.
+$p{"divseln"} = 100; #Number of divergently selected alleles
+$p{"divsels"} = $p{"totaldiv"}/$p{"divseln"}; #Strength of selection at divergently selected alleles
+$p{"bdmn"} = 10; #Number of BDM alleles, must be divisible by 2
+$p{"bdms"} = 0.0; #Strength of selection at BDMs
+$p{"mutationrate"} = 1e-7; #Mutation rate for adaptive alleles
+$p{"qtlsd"} = 1; #Standard deviation of qtl 
+$p{"fitnesssd"} = 2.0; #Standard deviation of fitness landscape.
+$p{"delta"} = 1; #How fast the optimum shifts after the burn in
+my $reps =1;  #Number of repetitions per set of parameters
+$p{"burningen"} = 10000; #Number of generations of burn in before shift
+$p{"shiftgen"} = 100; #Number of generations of shifting optimum.
+$p{"recombinationrate"} = 1e-5; #Number of generations of shifting optimum.
 my $max_runs = 100; #Maximum number of replicates before it starts skipping.
 my $output_dir = "output";
 
@@ -37,37 +37,40 @@ my %starting_p; #The starting point for incrementing;
 my %ending_p; #The ending point
 my %increment_p; #The amount it increases with each increment
 #PARAMETERS TO VARY
-#$varying_p{delta}++;
-#$varying_p{qtl_sd}++;
-#$varying_p{mutation_rate}++;
-#$varying_p{total_div}++;
-$varying_p{m}++;
-#$varying_p{div_sel_n}++;
+$varying_p{"delta"}++;
+$varying_p{"qtlsd"}++;
+$varying_p{"mutationrate"}++;
+$varying_p{"totaldiv"}++;
+$varying_p{"m"}++;
+$varying_p{"divseln"}++;
+$varying_p{"recombinationrate"}++;
 
 #Starting point of parameters
-#$starting_p{delta}= 0.1;
-#$starting_p{qtl_sd}= 0.1;
-#$starting_p{mutation_rate} = 1e-8;
-#$starting_p{total_div} = 0.1;
-$starting_p{m} = 0.0;
-#$starting_p{div_sel_n} = 5;
+$starting_p{"delta"}= 0.1;
+$starting_p{"qtlsd"}= 0.1;
+$starting_p{"mutationrate"} = 1e-8;
+$starting_p{"totaldiv"} = 0.1;
+$starting_p{"m"} = 0.0;
+$starting_p{"divseln"} = 5;
+$starting_p{"recombinationrate"} = 1e-5;
 
 #Ending point of parameters
-#$ending_p{delta}= 3;
-#$ending_p{qtl_sd}= 5;
-#$ending_p{mutation_rate} = 5e-7;
-#$ending_p{total_div} = 1;
-$ending_p{m} = 0.0;
-#$ending_p{div_sel_n} = 100;
+$ending_p{"delta"}= 3;
+$ending_p{"qtlsd"}= 5;
+$ending_p{"mutationrate"} = 5e-7;
+$ending_p{"totaldiv"} = 1;
+$ending_p{"m"} = 0.1;
+$ending_p{"divseln"} = 100;
+$ending_p{"recombinationrate"} = 5e-5;
 
 #Increment for parameters
-#$increment_p{delta}= 0.01;
-#$increment_p{qtl_sd}= 0.1;
-#$increment_p{mutation_rate} = 1e-8;
-#$increment_p{total_div} = 0.01;
-$increment_p{m} = 0.001;
-#$increment_p{div_sel_n} = 5;
-
+$increment_p{"delta"}= 0.01;
+$increment_p{"qtlsd"}= 0.1;
+$increment_p{"mutationrate"} = 1e-8;
+$increment_p{"totaldiv"} = 0.01;
+$increment_p{"m"} = 0.001;
+$increment_p{"divseln"} = 5;
+$increment_p{"recombinationrate"} = 1e-6;
 
 
 
@@ -85,8 +88,8 @@ foreach my $varying_parameter (sort keys %varying_p){
 	for (my $new_p = $starting_p{$varying_parameter}; $new_p <= $ending_p{$varying_parameter}; $new_p += $increment_p{$varying_parameter} ){ 
 		my %tmp_p = %p;
 		$tmp_p{$varying_parameter} = $new_p;
-		if ($tmp_p{m} > 0.5){next;} #Skip if migration is greater than panmixis.
-		$tmp_p{div_sel_s} = $tmp_p{total_div}/$tmp_p{div_sel_n};
+		if ($tmp_p{"m"} > 0.5){next;} #Skip if migration is greater than panmixis.
+		$tmp_p{"divsels"} = $tmp_p{"totaldiv"}/$tmp_p{"divseln"};
 		foreach my $rep (1..$reps){
 			$pm->start and next;
 			srand();
@@ -114,12 +117,14 @@ foreach my $varying_parameter (sort keys %varying_p){
 			print STDERR "Running burn in for $filename\n";
 			my $command_1 = "$slim";
                         foreach my $parameter (@parameters){
-                                $command_1 .= " -d d_${parameter}=$tmp_p{$parameter}";
+                                $command_1 .= " -d d${parameter}=$tmp_p{$parameter}";
                         }
-			$command_1 .= " -d d_rep=$rep";
-			$command_1 .= " -d d_seed=$seed";
-			$command_1 .= ' -d d_version=\"burn\" adaptive_introgression_WF_BDM_CMD.slim';
+			$command_1 .= " -d drep=$rep";
+			$command_1 .= " -d dseed=$seed";
+			$command_1 .= ' -d dversion=\"burn\" adaptive_introgression_WF_BDM_CMD.slim';
 			my $output_1 = `$command_1`;
+#			my $output_1 = "NA";
+#			print "$command_1\n";
 			chomp($output_1);
                         my @lines = split(/\n/,$output_1);
                         foreach my $line (@lines){
@@ -134,12 +139,14 @@ foreach my $varying_parameter (sort keys %varying_p){
 			print STDERR "Running test shift for $filename\n";
                         my $command_2 = "$slim";
                         foreach my $parameter (@parameters){
-                                $command_2 .= " -d d_${parameter}=$tmp_p{$parameter}";
+                                $command_2 .= " -d d${parameter}=$tmp_p{$parameter}";
                         }
-			$command_2 .= " -d d_rep=$rep";
-                        $command_2 .= " -d d_seed=$seed";
-                        $command_2 .= ' -d d_version=\"test\" adaptive_introgression_WF_BDM_CMD.slim';
+			$command_2 .= " -d drep=$rep";
+                        $command_2 .= " -d dseed=$seed";
+                        $command_2 .= ' -d dversion=\"test\" adaptive_introgression_WF_BDM_CMD.slim';
 			my $output_2 = `$command_2`;
+#			my $output_2 = "NA";
+#			print "$command_2\n";
 			chomp($output_2);
 			@lines = split(/\n/,$output_2);
                         foreach my $line (@lines){
@@ -151,20 +158,22 @@ foreach my $varying_parameter (sort keys %varying_p){
 					print $fh3 "\n$line";
 				}
 			}
-			my $longer_gen = $tmp_p{burn_in_gen} + $tmp_p{shift_gen};
+			my $longer_gen = $tmp_p{burningen} + $tmp_p{shiftgen};
 			print STDERR "Running control for $filename\n";
                         my $command_3 = "$slim";
                         foreach my $parameter (@parameters){
-				if ($parameter eq "burn_in_gen"){next;}
-				if ($parameter eq "shift_gen"){next;}
-                                $command_3 .= " -d d_${parameter}=$tmp_p{$parameter}";
+				if ($parameter eq "burningen"){next;}
+				if ($parameter eq "shiftgen"){next;}
+                                $command_3 .= " -d d${parameter}=$tmp_p{$parameter}";
                         }
-			$command_3 .= " -d d_shift_gen=0";
-			$command_3 .= " -d d_burn_in_gen=$longer_gen";
-			$command_3 .= " -d d_rep=$rep";
-                        $command_3 .= " -d d_seed=$seed";
-                        $command_3 .= ' -d d_version=\"control\" adaptive_introgression_WF_BDM_CMD.slim';
+			$command_3 .= " -d dshiftgen=0";
+			$command_3 .= " -d dburningen=$longer_gen";
+			$command_3 .= " -d drep=$rep";
+                        $command_3 .= " -d dseed=$seed";
+                        $command_3 .= ' -d dversion=\"control\" adaptive_introgression_WF_BDM_CMD.slim';
                         my $output_3 = `$command_3`;
+#			my $output_3 = "NA";
+#			print "$command_3\n";
 			chomp($output_3);
 			@lines = split(/\n/,$output_3);
 
